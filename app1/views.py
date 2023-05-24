c14cfcefcb617d028fe90548834faf9b0417bf0c
PraveenKumar1014 <praveenreddypogula14@gmail.com>
2023-05-24 21:41:09 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def app1_string(request):
+    return HttpResponse('<h1>this is my app1_string</h1>')
+def app1_htmlpage(request):
+    return render(request,'app1_htmlpage.html')
+
